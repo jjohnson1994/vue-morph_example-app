4a04f5565b81f49dcefd9e62b9ac19e7ef201690
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2018-07-01 16:04:23 +0100

    Add Build android CLI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import VueMorph from 'vue-morph';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMorph);
 
 new Vue({
   router,
