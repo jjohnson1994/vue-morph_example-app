8bf8c34fa221c0731a15baba210e78470de65d30
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2018-11-25 11:49:28 +0000

    Vue Github Commits Reader

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,70 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <ScrollView id="app">
+    <LinearLayout>
+      <TextView :text="pageTitle"></TextView>
+
+      <RadioGroup name="branch" v-model="currentBranch" :options="branches"></RadioGroup>
+    
+      <TextView :text="'vuejs/vue@' + currentBranch"></TextView>
+      
+      <LinearLayout>
+        <TextView
+          v-for="record in commits"
+          :key="record.sha"
+          :text="record.sha.slice(0, 7) + ', ' + record.commit.message + ', ' + record.commit.author.name"
+        ></TextView>
+      </LinearLayout>
+    </LinearLayout>
+  </ScrollView>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+const apiURL = 'https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha=';
+
+export default {
+  name: 'App',
+  data(): object {
+    return {
+      pageTitle: 'Vue Morph',
+      branches: ['master', 'dev'],
+      currentBranch: 'master',
+      commits: [],
+    };
+  },
+  created(): void {
+    window.setTimeout(() => {
+      this.fetchData();
+    }, 1000);
+  },
+  watch: {
+    currentBranch() {
+      this.fetchData();
+    },
+  },
+  methods: {
+    sayHello(message: string) {
+      Android.showToast(message);
+    },
+
+    fetchData(): void {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = () => {
+        this.commits = JSON.parse(xhr.responseText);
+      };
+      xhr.open('GET', apiURL + this.currentBranch);
+      xhr.send();
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
